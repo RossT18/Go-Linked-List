53f0a19755e00586c308db08f9276b441c42bc64
Ross Tinsley <rtinsley1199@gmail.com>
2020-05-14 02:27:58 +0100

    Basic insertion

--- linked-list.go
+++ linked-list.go
@@ -7,7 +7,7 @@ type Node struct {
 	next  *Node
 }
 
-func addNode(head *Node, v int) {
+func push(head *Node, v int) {
 	newNode := &Node{value: v}
 	if head.next == nil {
 		head.next = newNode
@@ -22,7 +22,7 @@ func addNode(head *Node, v int) {
 
 func showNodes(head *Node) {
 	current := head
-	i := 1
+	i := 0
 	for current.next != nil {
 		fmt.Println(i, ":", current.value)
 		current = current.next
@@ -31,15 +31,34 @@ func showNodes(head *Node) {
 	fmt.Println(i, ":", current.value)
 }
 
-func main() {
+func insert(head *Node, v, p int) {
+	current := head
+	i := 0
+	for current.next != nil {
+		if i == p-1 {
+			// Found the position of the previous node.
+			// Change this one's next and if it had a next, use that in the new node's next.
+			// Also break out of the loop. No point in carrying on.
+			oldNext := current.next
+			newNode := &Node{value: v, next: oldNext}
+			current.next = newNode
+			break
+		}
+		current = current.next
+		i++
+	}
 
+}
+
+func main() {
 	head := Node{
 		value: 10,
 	}
 
 	for i := 1; i < 10; i++ {
-		addNode(&head, i+10)
+		push(&head, i+10)
 	}
 
+	insert(&head, 20, 2)
 	showNodes(&head)
 }
